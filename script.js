14a1b0239e79e324725d9f0dd18788c32b24b88e
Your Name <you@example.com>
2023-07-16 21:07:00 +0100

    implementing third party map library

--- script.js
+++ script.js
@@ -10,4 +10,30 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
- 
+
+if (navigator.geolocation.getCurrentPosition)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/place/Nsukka/@${latitude},${longitude}`
+      );
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not access your current location');
+    }
+  );
